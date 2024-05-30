cb692d727aaca0d08087dd344a88a8b79e12173f
Fabian Keller <github.100.fkeller@spamgourmet.com>
2024-05-30 22:59:36 +0200

    few more flutter experiments

--- frb_test_1/lib/main.dart
+++ frb_test_1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:frb_test_1/src/rust/api/simple.dart';
 import 'package:frb_test_1/src/rust/frb_generated.dart';
@@ -5,6 +7,17 @@ import 'package:frb_test_1/src/rust/frb_generated.dart';
 Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
+}
+
+final Future<String> _calculation = Future<String>.delayed(
+  const Duration(seconds: 2),
+  () => 'Data Loaded',
+);
+
+Future<List<String>> _getFiles() async {
+  final dir = Directory('/home/fabian');
+  final List<FileSystemEntity> entities = await dir.list().toList();
+  return entities.map((e) => e.path).toList();
 }
 
 class MyApp extends StatelessWidget {
@@ -15,10 +28,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-              'Action: Calling into Rust...\nResult: ${greet(name: "InputName")}'),
-        ),
+        body: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text(
+                  'List item $index',
+                  style: Theme.of(context).textTheme.bodySmall,
+                );
+              },
+            ),
+          ),
+          Text(
+            'Action: Calling into Rust...\nResult: ${greet(name: "InputName")}',
+          ),
+          FutureBuilder<List<String>>(
+            future: _getFiles(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+              List<Widget> children;
+              if (snapshot.hasData) {
+                var files = snapshot.data!..sort();
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: files.length,
+                    itemBuilder: (context, index) {
+                      return Text(
+                        files[index],
+                        style: Theme.of(context).textTheme.bodySmall,
+                      );
+                    },
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                children = <Widget>[
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  ),
+                ];
+              } else {
+                children = const <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  ),
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            },
+          )
+        ]),
       ),
     );
   }
